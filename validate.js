239e8ff7b3f52f8ae104591c90edb42b943cd875
Raguwarman <raguwarman1999@gmail.com>
2022-01-11 15:59:58 +0530

    Final commit

---
+++ validate.js
@@ -0,0 +1,11 @@
+<script language="Javascript">
+    function IsEmpty() {
+if (document.form.Name.value == "") {
+        alert("empty");
+      }
+      if (document.form.RegNo.value == "") {
+        alert("empty");
+      }
+      return;
+    }
+  </script>
